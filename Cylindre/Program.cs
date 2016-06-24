3691f6c6cc41a88650daa21051537c06e5d8752b
joce <jocelynlegault@gmail.com>
2016-06-24 00:22:45 -0400

    Change geosphere to use same writing as sphere
    Signed-off-by: joce <jocelynlegault@gmail.com>

--- Cylindre/Program.cs
+++ Cylindre/Program.cs
@@ -123,26 +123,26 @@ namespace Cylindre
             indices.AddRange(
                 new []
                 {
-                    0,4,1,
-                    0,9,4,
-                    9,5,4,
-                    4,5,8,
-                    4,8,1,
-                    8,10,1,
-                    8,3,10,
-                    5,3,8,
-                    5,2,3,
-                    2,7,3,
-                    7,10,3,
-                    7,6,10,
-                    7,11,6,
-                    11,0,6,
-                    0,1,6,
-                    6,1,10,
-                    9,0,11,
-                    9,11,2,
-                    9,2,5,
-                    7,2,11
+                    0,1,4,
+                    0,4,9,
+                    9,4,5,
+                    4,8,5,
+                    4,1,8,
+                    8,1,10,
+                    8,10,3,
+                    5,8,3,
+                    5,3,2,
+                    2,3,7,
+                    7,3,10,
+                    7,10,6,
+                    7,6,11,
+                    11,6,0,
+                    0,6,1,
+                    6,10,1,
+                    9,11,0,
+                    9,2,11,
+                    9,5,2,
+                    7,11,2
                 }
             );
 
@@ -199,7 +199,7 @@ namespace Cylindre
 
                 for (int i = 0; i < indices.Count; i+=3)
                 {
-                    sr.WriteLine("f {0}//{0} {1}//{1} {2}//{2}", indices[i]+1, indices[i+2]+1, indices[i+1]+1);
+                    sr.WriteLine("f {0}//{0} {1}//{1} {2}//{2}", indices[i]+1, indices[i+1]+1, indices[i+2]+1);
                 }
             }
         }
@@ -285,8 +285,6 @@ namespace Cylindre
                 indices.AddRange(new[] { i+1, bottomIdx, i });
             }
             indices.AddRange(new[] { n*(n/2-2)+1, bottomIdx, n*(n/2-1) });
-
-
 
 
             using (FileStream fs = File.Open(@"c:\dump\sphere.obj", FileMode.Create))
